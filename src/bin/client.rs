8731d36c867b1d48b16abdec257f78d6afb443b9
Andrei <eu@andreih.com>
2021-04-11 15:07:45 -0300

    Add a CLI interface to intereact with the server

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,13 +1,41 @@
+use clap::{App, Arg};
 use kv_store::{client::send_request, Request};
 
 #[tokio::main]
 pub async fn main() {
-    let response = send_request(Request::Put(
-        String::from("Name"),
-        String::from("Andrei"),
-        None,
-    ))
-    .await;
+    let matches = App::new("kv_store_client")
+        .subcommand(
+            App::new("put")
+                .arg(Arg::new("KEY").required(true))
+                .arg(Arg::new("VALUE").required(true))
+                .arg(Arg::new("SECONDS_TO_EXPIRE")),
+        )
+        .subcommand(App::new("get").arg(Arg::new("KEY").required(true)))
+        .subcommand(App::new("delete").arg(Arg::new("KEY").required(true)))
+        .get_matches();
 
-    println!("Server responded: {:?}", response);
+    let maybe_request = match matches.subcommand() {
+        Some(("put", sub_m)) => {
+            let key = sub_m.value_of("KEY").unwrap();
+            let value = sub_m.value_of("VALUE").unwrap();
+            let expiration: Option<i32> = sub_m
+                .value_of("SECONDS_TO_EXPIRE")
+                .map(|seconds| seconds.parse().expect("Invalid number"));
+
+            Some(Request::Put(key.to_string(), value.to_string(), expiration))
+        }
+        Some(("get", sub_m)) => {
+            let key = sub_m.value_of("KEY").unwrap();
+            Some(Request::Get(key.to_string()))
+        }
+        Some(("delete", sub_m)) => {
+            let key = sub_m.value_of("KEY").unwrap();
+            Some(Request::Delete(key.to_string()))
+        }
+        _ => None,
+    };
+
+    if let Some(request) = maybe_request {
+        println!("Server responded: {:?}", send_request(request).await);
+    };
 }
